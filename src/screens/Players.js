ec7ff185a222ee20ef6322c694fd074241bba3ba
snow <matt01010000@gmail.com>
2023-09-07 00:25:30 -0400

    Added button to players screen

--- src/screens/Players.js
+++ src/screens/Players.js
@@ -1,11 +1,22 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
+const EditPlayers = ({ onPress }) => (
+  <TouchableOpacity style={styles.button} onPress={onPress}>
+    <Text style={styles.buttonText}>Add icon instead of text</Text>
+  </TouchableOpacity>
+);
+
 export default function Players() {
+  const editPlayers2 = () => {
+    console.log("Changing Players");
+  };
+
   return (
     <View style={styles.container}>
       <Text>Players</Text>
       <StatusBar style="auto" />
+      <EditPlayers onPress={editPlayers2} />
     </View>
   );
 }
@@ -16,5 +27,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#1518E3",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
