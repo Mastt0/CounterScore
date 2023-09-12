1ccaea4ad3cd3fdf9de6d3d871df075fe9bee309
snow <matt01010000@gmail.com>
2023-09-12 19:59:42 -0400

    Added GameScore page to drawer navigation

--- src/Navigation/DrawerNavigator.js
+++ src/Navigation/DrawerNavigator.js
@@ -1,5 +1,6 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../screens/Home";
+import GameScore from "../screens/GameScore";
 import Players from "../screens/Players";
 
 const Drawer = createDrawerNavigator();
@@ -8,6 +9,7 @@ export default function DrawerNavigator() {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Current Game" component={GameScore} />
       <Drawer.Screen name="Players" component={Players} />
     </Drawer.Navigator>
   );
