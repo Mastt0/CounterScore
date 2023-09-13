7703ec7a5b0aec4d05466a99069cffca9dad2d7f
snow <matt01010000@gmail.com>
2023-09-12 20:47:12 -0400

    Changed View Orientation to Landscape for App

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import DrawerNavigator from "./src/Navigation/DrawerNavigator";
+import React, { useEffect } from "react";
+import * as ScreenOrientation from "expo-screen-orientation";
 
 export default function App() {
+  //Used to Set Orientation to Landscape
+  useEffect(() => {
+    async function setOrientation() {
+      await ScreenOrientation.lockAsync(
+        ScreenOrientation.OrientationLock.LANDSCAPE
+      );
+    }
+    setOrientation();
+  }, []);
+
   return (
+    //Renders the Drawer Navigation which can be found in the counterScore/src/Navigation Directory
     <NavigationContainer>
       <DrawerNavigator />
     </NavigationContainer>
